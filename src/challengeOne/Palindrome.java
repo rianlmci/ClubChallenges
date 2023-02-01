b3b1e7f0502c4334d93e455063aa2c3f4bf50e49
rianlmci <winged_zinzan@hotmail.com>
2023-02-01 10:53:02 -0700

    Syntax cleanup. Removed spaces between char and [].

--- src/challengeOne/Palindrome.java
+++ src/challengeOne/Palindrome.java
@@ -10,7 +10,6 @@ public class Palindrome {
      * Checks if a character array contains a palindrome
      * @param palindromeArray array to be checked
      * @return <code>true</code> if array contains a palindrome, otherwise returns <code>false</code>.
-     *
      */
     public static boolean isPalindrome(char[] palindromeArray){
         return false; //TODO
@@ -25,22 +24,21 @@ public class Palindrome {
     public static void palindromeTester(char[] palindromeArray){
         System.out.printf("%s is a palindrome .. %b! \n",
                 String.valueOf(palindromeArray), isPalindrome(palindromeArray));
-
     }
 
-    // = = = = = TEST CLIENT = = = = = =//
-    public static void main(String [] args){
-        char [] palindromeOne = new char [] {'t','a','c','o','c','a','t'}; //tacocat
-        char [] palindromeTwo = new char [] {'c','i','v','i','c'}; //civic
-        char [] palindromeThree = new char [] {'r','o','t','a','t','o','r'}; //rotator
-        char [] palindromeFour = new char [] {'r','a','c','e','c','a','r'}; //racecar
-        char [] palindromeFive = new char [] {'l','i','e','s'}; //lies ... NOT A PALINDROME!
+    // = = = = = TEST CLIENT = = = = = = //
+    public static void main(String[] args){
+        char[] palindromeOne = new char[] {'t','a','c','o','c','a','t'}; //tacocat
+        char[] palindromeTwo = new char[] {'c','i','v','i','c'}; //civic
+        char[] palindromeThree = new char[] {'r','o','t','a','t','o','r'}; //rotator
+        char[] palindromeFour = new char[] {'r','a','c','e','c','a','r'}; //racecar
+        char[] palindromeFive = new char[] {'l','i','e','s'}; //lies ... NOT A PALINDROME!
 
-        char [][] allPalindromes = new char [] [] {
+        char[][] allPalindromes = new char[][] {
               palindromeOne, palindromeTwo, palindromeThree, palindromeFour, palindromeFive
         };
 
-        for (char [] palindrome : allPalindromes) {
+        for (char[] palindrome : allPalindromes) {
             palindromeTester(palindrome);
         }
     }
